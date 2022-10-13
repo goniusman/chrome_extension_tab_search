a93eca11c20b0d481febc118cd9edf18907c2b79
goniusman <goniusman400@gmail.com>
2022-10-13 17:02:49 +0600

    all text content

--- background.js
+++ background.js
@@ -1,85 +1,105 @@
 
 
-// chrome.windows.getAll({populate:true}, getAllOpenWindows);
-// function getAllOpenWindows(winData) {
-//   var tabs = [];
-//   for (var i in winData) {
-//     if (winData[i].focused === true) {
-//         var winTabs = winData[i].tabs;
-//         var totTabs = winTabs.length;
-//         for (var j=0; j<totTabs;j++) {
-//           tabs.push(winTabs[j].url);
-//         }
-//     }
-//   }
-//   console.log(winData);
-// }
 
 
-// {
-//   "active": false,
-//   "audible": false,
-//   "autoDiscardable": true,
-//   "discarded": false,
-//   "favIconUrl": "https://ssl.gstatic.com/ui/v1/icons/mail/rfr/gmail.ico",
-//   "groupId": -1,
-//   "height": 937,
-//   "highlighted": false,
-//   "id": 1596125134,
-//   "incognito": false,
-//   "index": 1,
-//   "mutedInfo": {
-//       "muted": false
-//   },
-//   "pinned": false,
-//   "selected": false,
-//   "status": "complete",
-//   "title": "Inbox - goniusman600@gmail.com - Gmail",
-//   "url": "https://mail.google.com/mail/u/0/#inbox",
-//   "width": 1920,
-//   "windowId": 1596125132
-// }
+  // chrome.windows.getAll({populate:true}, getAllOpenWindows);
+  // function getAllOpenWindows(winData) {
+  //   var tabs = [];
+  //   for (var i in winData) {
+  //     if (winData[i].focused === true) {
+  //         var winTabs = winData[i].tabs;
+  //         var totTabs = winTabs.length;
+  //         for (var j=0; j<totTabs;j++) {
+  //           tabs.push(winTabs[j].url);
+  //         }
+  //     }
+  //   }
+  //   console.log(winData);
+  // }
 
 
-chrome.tabs.query({ currentWindow: true }, function (tabs) {
-  let search = tabs.filter(function (tab) {
-   return tab.url.split("?")[0].substr("8").split('.')[0].includes('m');
-});
+  // {
+  //   "active": false,
+  //   "audible": false,
+  //   "autoDiscardable": true,
+  //   "discarded": false,
+  //   "favIconUrl": "https://ssl.gstatic.com/ui/v1/icons/mail/rfr/gmail.ico",
+  //   "groupId": -1,
+  //   "height": 937,
+  //   "highlighted": false,
+  //   "id": 1596125134,
+  //   "incognito": false,
+  //   "index": 1,
+  //   "mutedInfo": {
+  //       "muted": false
+  //   },
+  //   "pinned": false,
+  //   "selected": false,
+  //   "status": "complete",
+  //   "title": "Inbox - goniusman600@gmail.com - Gmail",
+  //   "url": "https://mail.google.com/mail/u/0/#inbox",
+  //   "width": 1920,
+  //   "windowId": 1596125132
+  // }
 
-  // console.log(search[0].selected);
-  // search[0].selected = true;
-  // search[0].active = true;
-  // search[0].highlighted = true;
-  // console.log(search[0].selected);
 
-// console.log(tabs);
 
-  // chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-  //     your_tab_Id = sender.tab.id;
-  //     chrome.tabs.update(your_tab_Id,{"active":true,"highlighted":true},function (tab){
-  //         console.log("Completed updating tab .." + JSON.stringify(tab));
-  //     });
+
+
+  // chrome.tabs.query({ currentWindow: true }, function (tabs) {
+  //   let search = tabs.filter(function (tab) {
+  //   return tab.url.split("?")[0].substr("8").split('.')[0].includes('m');
   // });
 
-  // chrome.tabs.update(1596125322,{"active":true,"highlighted":true},function (tab){
-  //   console.log("Completed updating tab .." + JSON.stringify(tab));
-  // }); 
+    // console.log(search[0].selected);
+    // search[0].selected = true;
+    // search[0].active = true;
+    // search[0].highlighted = true;
+    // console.log(search[0].selected);
 
-  // search.forEach(element => {
-  //   // console.log(element.favIconUrl);
-  //   // console.log(element.title);
-  //   // console.log(element.windowId);
+    // console.log(tabs);
 
-  //   element.active = true;
-  //   element.selected = true;
+    // chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+    //     your_tab_Id = sender.tab.id;
+    //     chrome.tabs.update(your_tab_Id,{"active":true,"highlighted":true},function (tab){
+    //         console.log("Completed updating tab .." + JSON.stringify(tab));
+    //     });
+    // });
+
+    ////find tab id
+    // chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
+    //     your_tab_Id = sender.tab.id);
+    // });
 
 
-  // });
-  // console.log(all);
+    ///////// use instead of onRequest 
+    //   chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+    //     your_tab_Id = sender.tab.id;
+    //     chrome.tabs.update(your_tab_Id,{"active":true,"highlighted":true},function (tab){
+    //         console.log("Completed updating tab .." + JSON.stringify(tab));
+    //     });
+    // })
+
+
+    // chrome.tabs.update(1596125322,{"active":true,"highlighted":true},function (tab){
+    //   console.log("Completed updating tab .." + JSON.stringify(tab));
+    // }); 
+
+    // search.forEach(element => {
+    //   // console.log(element.favIconUrl);
+    //   // console.log(element.title);
+    //   // console.log(element.windowId);
+
+    //   element.active = true;
+    //   element.selected = true;
+
+
+    // });
+    // console.log(all);
 
 
   
-});
+  // });
 
 
 
@@ -89,10 +109,23 @@ chrome.tabs.query({ currentWindow: true }, function (tabs) {
 
 
 
+//// find text
+  // let do = document.getElementsByClassName('question-page');
 
+  // function deepText(node){
+  //     var A= [];
+  //     if(node){
+  //         node= node.firstChild;
+  //         while(node!= null){
+  //             if(node.nodeType== 3) A[A.length]=node;
+  //             else A= A.concat(deepText(node));
+  //             node= node.nextSibling;
+  //         }
+  //     }
+  //     return A;
+  // }
 
-
-
+  // deepText(dos[0])
 
 
 
